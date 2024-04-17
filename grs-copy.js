8fbb540493e33ba18f83e07a9b67b3972fc77c71
Ceren Ekerbiçer <96716340+cernekr@users.noreply.github.com>
2024-04-17 12:58:51 +0300

    Add files via upload

---
+++ grs-copy.js
@@ -0,0 +1,144 @@
+function myMenuFunction() {
+  var menuBtn = document.getElementById("myNavM");
+
+  if (menuBtn.className === "nav-menu") {
+      menuBtn.className += " responsive";
+  } else {
+      menuBtn.className = "nav-menu";
+  }
+}
+
+window.onscroll = function() {
+  headerShadow();
+};
+
+function headerShadow() {
+  const navHeader = document.getElementById("header");
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+
+      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+      navHeader.style.height = "70px";
+      navHeader.style.lineHeight = "70px";
+
+  } else {
+
+      navHeader.style.boxShadow = "none";
+      navHeader.style.height = "90px";
+      navHeader.style.lineHeight = "90px";
+
+  }
+}
+
+var typingEffect = new Typed(".typedText", {
+  strings: ["Tasarımcıyım", "Organizatörüm", "Yazılımcıyım"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000
+});
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
+sr.reveal('.featured-text-card', {});
+sr.reveal('.featured-name', {
+  delay: 100
+});
+sr.reveal('.featured-text-info', {
+  delay: 200
+});
+sr.reveal('.featured-text-btn', {
+  delay: 200
+});
+sr.reveal('.social_icons', {
+  delay: 200
+});
+sr.reveal('.featured-image', {
+  delay: 300
+});
+
+sr.reveal('.project-box', {
+  interval: 200
+});
+
+sr.reveal('.top-header', {});
+
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
+srLeft.reveal('.about-info', {
+  delay: 100
+});
+srLeft.reveal('.contact-info', {
+  delay: 100
+});
+
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
+srRight.reveal('.skills-box', {
+  delay: 100
+});
+srRight.reveal('.form-control', {
+  delay: 100
+});
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach(current => {
+      const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 50,
+          sectionId = current.getAttribute('id');
+
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          document.querySelector('.nav-menu a[href="#' + sectionId + '"]').classList.add('active-link');
+      } else {
+          document.querySelector('.nav-menu a[href="#' + sectionId + '"]').classList.remove('active-link');
+      }
+  });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+
+
+document.getElementById("dropdownbtn").addEventListener("change", function() {
+  var selectedLanguage = this.value;
+  if (selectedLanguage === "English") {
+      showEnglishText();
+  } else if (selectedLanguage === "Türkçe") {
+      showTurkishText();
+  }
+});
+
+function showEnglishText() {
+  document.getElementById("title").textContent = "My Cv";
+  document.getElementById("Anasayfa").textContent = "Home";
+  document.getElementById("hakkında").textContent = "About";
+  document.getElementById("Çalışmalarım").textContent = "My Project";
+  document.getElementById("İletişim").textContent = "Contact";
+}
+
+function showTurkishText() {
+  document.getElementById("title").textContent = "Portfolyo'm!";
+  document.getElementById("Anasayfa").textContent = "Anasayfa";
+  document.getElementById("hakkında").textContent = "Hakkımda";
+  document.getElementById("Çalışmalarım").textContent = "Çalışmalarım";
+  document.getElementById("İletişim").textContent = "İletişim";
+}
